bfaa5ddc5ebb2dadac02a697f92e3fd6e7e4fd6e
Leo <icpc0928@yahoo.com.tww>
2021-08-01 01:23:42 +0800

    2021/07/26-html 連結 新增sql th方法 表單到controller的路徑

--- src/main/java/com/leo/springbootdemo/web/BookController.java
+++ src/main/java/com/leo/springbootdemo/web/BookController.java
@@ -8,6 +8,9 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.List;
 
 @Controller
 public class BookController {
@@ -15,18 +18,53 @@ public class BookController {
     @Autowired
     private BookService bookService;
 
+    /**
+     * 獲取書單列表
+     * @param model
+     * @return
+     */
     @GetMapping("/books")
-    public String list(){
+    public String list(Model model){
+        List<Book> books = bookService.findAll();
+        model.addAttribute("books", books);
         return "books";
     }
 
-
+    /**
+     * 獲取書單詳情
+     * @param id
+     * @param model
+     * @return
+     */
     //返回模板的名字
     @GetMapping("/books/{id}")
     public String detail(@PathVariable long id, Model model){
         Book book = bookService.findById(id);
+        //回傳物件名稱, 物件
         model.addAttribute("book", book);
+        //因此類別為Controller 故回傳值會導向book.html
         return "book";
+    }
+
+    /**
+     * 跳轉input提交葉面
+     * @return
+     */
+    @GetMapping("/books/input")
+    public String inputPage(){
+        return "input";
+    }
+
+    /**
+     * 提交一個書單信息。   redirect 返回books頁面
+     * redirect: 重定向路徑
+     * @param book
+     * @return
+     */
+    @PostMapping("/books")
+    public String post(Book book){
+        bookService.save(book);
+        return "redirect:/books";
     }
 
 }
